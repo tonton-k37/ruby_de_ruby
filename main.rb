eafc6f06b1b5fcb45d6fc06229d83043c484dbf6
kyohei <tonton.k.pg@gmail.com>
2022-02-11 13:56:07 +0900

    initial commit

---
+++ main.rb
@@ -0,0 +1,30 @@
+
+
+leaf_a = ["leaf a"]
+leaf_b = ["leaf b"]
+leaf_c = ["leaf c"]
+leaf_d = ["leaf d"]
+
+node2 = ["fushi 2", leaf_a, leaf_b]
+
+node3 = ["fushi 3", leaf_c, leaf_d]
+
+node1 = ["fushi 1", node2, node3]
+
+# 
+# node1
+# node2 node3
+# leaf_a leaf_b, leaf_c leaf_d
+#
+
+def preorder(tree)
+    tree.each {|branch| branch.respond_to?(:each) ? preorder(branch) : p(branch)} 
+end
+
+preorder(node1)
+
+def reversive(tree)
+    tree.reverse.each {|branch| branch.respond_to?(:each) ? reversive(branch) : p(branch)}
+end
+
+reversive(node1)
