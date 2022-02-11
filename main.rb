256c252402a8bfde4e59afedc74064e8c4714435
kyohei <tonton.k.pg@gmail.com>
2022-02-11 17:43:14 +0900

    四則演算スクリプト実装

--- main.rb
+++ main.rb
@@ -1,30 +1,33 @@
+# frozen_string_literal: true
 
+require './minruby'
 
-leaf_a = ["leaf a"]
-leaf_b = ["leaf b"]
-leaf_c = ["leaf c"]
-leaf_d = ["leaf d"]
+text = minruby_parse('(1 + 2) / 3 * 4 * (56 / 7 + 8 + 9)')
 
-node2 = ["fushi 2", leaf_a, leaf_b]
+def evaluate(tree)
+  # litで始まっている場合は四則演算記号ではないの
+  # かつ、節ではなく葉なので合計する必要ないためそのまま返す
+  # それ以外は再起関数で繰り返し
 
-node3 = ["fushi 3", leaf_c, leaf_d]
+  return tree[1] if tree[0] == 'lit'
 
-node1 = ["fushi 1", node2, node3]
-
-# 
-# node1
-# node2 node3
-# leaf_a leaf_b, leaf_c leaf_d
-#
-
-def preorder(tree)
-    tree.each {|branch| branch.respond_to?(:each) ? preorder(branch) : p(branch)} 
+  left = evaluate(tree[1])
+  right = evaluate(tree[2])
+  begin
+    case tree[0]
+    when '+'
+      left + right
+    when '-'
+      left - right
+    when '*'
+      left * right
+    else # /
+      left / right
+    end
+  rescue StandardError => e
+    puts 'Oh my goooooood :X'
+  end
 end
 
-preorder(node1)
-
-def reversive(tree)
-    tree.reverse.each {|branch| branch.respond_to?(:each) ? reversive(branch) : p(branch)}
-end
-
-reversive(node1)
+result = evaluate(text)
+puts(result)
